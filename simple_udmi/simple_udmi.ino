a5d4b147265f7430b33d76515b2c31c462182f8b
Jason Brewer <jason@urawizard.com>
2019-07-31 15:14:23 +0100

    Initial commit

---
+++ simple_udmi/simple_udmi.ino
@@ -0,0 +1,45 @@
+# Tested with an Uno
+
+#include <ArduinoJson.h>
+int count = 0;
+int x = 2000;
+int y = 500;
+int bub = 0;
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+
+  int bib = digitalRead(2);
+  if (bib == HIGH && bub == LOW) {
+      digitalWrite(3, !digitalRead(3));
+  }
+  
+  int bob = analogRead(0);
+  analogWrite(5, 255-bob);
+
+  bub = bib;
+  
+  StaticJsonDocument<200> udmi_out;
+  
+  //  build udmi
+
+  udmi_out["version"] = 1;
+  udmi_out["timestamp"] = "0";
+  udmi_out["points"]["lux_level"]["present_value"] = bob;
+  udmi_out["points"]["lum_state"]["present_value"] = digitalRead(3)*100;
+  udmi_out["points"]["dimmer_value"]["present_value"] = 255-bob;
+
+  //  write udmi
+  
+  serializeJson(udmi_out, Serial);
+  Serial.println();
+  
+}
